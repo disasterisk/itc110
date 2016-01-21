e70d29b562353a749117ff5377cba3cabbb34ebd
disasterisk <disasterisk@users.noreply.github.com>
2016-01-20 20:34:52 -0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,5 +5,9 @@ name = input()
 print ('It\'s nice to meet you, '+ name)
 print ('Did you know that your name is '+str(len(name))+' letters long?')
 print ('How old are you?') # ask for age
-age = input()
-print ('That\'s like '+str(int(age)*7)+' in dog years.')
+age = int(input())
+if age < 2:
+    print("You are excellent with a command line for your age.")
+else:
+    dogAge = 21+((age-2)*4)
+    print ('That\'s like '+str(dogAge)+' in dog years.')
